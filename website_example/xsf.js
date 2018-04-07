b39aa9a68cd8d5dac7dee1eecbe4ea11255bdacc
0xA01 <cryptoape@icloud.com>
2018-04-07 22:05:39 +0300

    FlakeChain Pool frontend

---
+++ website_example/xsf.js
@@ -0,0 +1,17 @@
+var api = "http://snowflake.cf:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "snowflake.cf";
+
+var irc = "https://t.me/flakechain";
+
+var email = "cryptofoundry@icloud.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://snowflake.cf/";
+
+var transactionExplorer = "http://snowflake.cf/";
